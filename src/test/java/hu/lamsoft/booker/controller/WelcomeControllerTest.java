5843672c7c8e48b6697cc82935688e1eb54ab362
Csaba Lamfalusi <clamfalusi@hotels.com>
2021-03-04 23:32:10 +0100

    Adding Unit test for WelcomeController

---
+++ src/test/java/hu/lamsoft/booker/controller/WelcomeControllerTest.java
@@ -0,0 +1,24 @@
+package hu.lamsoft.booker.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.Model;
+
+public class WelcomeControllerTest {
+
+    @Test
+    public void testGreeting() {
+        // Given
+        WelcomeController underTest = new WelcomeController();
+        Model model = Mockito.mock(Model.class);
+        String expected = "welcome";
+        
+        // When
+        String actual = underTest.greeting(model);
+        
+        // Then
+        Assertions.assertEquals(expected, actual);
+    }
+    
+}
