78e76c005048796a2ecc69d3a198f7f86024170a
Csaba Lamfalusi <clamfalusi@hotels.com>
2021-03-07 18:46:54 +0100

    Add basic bootstrap design

--- src/main/java/hu/lamsoft/booker/configuration/WebSecurityConfiguration.java
+++ src/main/java/hu/lamsoft/booker/configuration/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package hu.lamsoft.booker.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +15,13 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+                .antMatchers("/css/**", "/js/**", "/assets/**", "/error");
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
